12035f49d284804cb2fe3597dea4d22a0e9a263a
Muff1n <253524507+muff1n-pixel@users.noreply.github.com>
2026-02-04 20:53:51 +0100

    chore: improve room customize tab

--- packages/game/src/UserInterface/components/Room/Settings/Tabs/RoomSettingsCustomizeTab.tsx
+++ packages/game/src/UserInterface/components/Room/Settings/Tabs/RoomSettingsCustomizeTab.tsx
@@ -40,57 +40,73 @@ export default function RoomSettingsCustomizeTab() {
 
             display: "flex",
             flexDirection: "column",
-            gap: 8
+            gap: 32,
         }}>
-            <b>Room customization</b>
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: 8
+            }}>
+                <b>Room customization</b>
 
-            <Selection value={floorThickness} items={[
-                {
-                    value: 0,
-                    label: "Thinnest floor"
-                },
-                {
-                    value: 4,
-                    label: "Thin floor"
-                },
-                {
-                    value: 8,
-                    label: "Normal floor"
-                },
-                {
-                    value: 12,
-                    label: "Thick floor"
-                },
-                {
-                    value: 16,
-                    label: "Thickest floor"
-                }
-            ]} onChange={(value) => setFloorThickness(value as number)}/>
-            
-            <Selection value={wallThickness} items={[
-                {
-                    value: 0,
-                    label: "Thinnest walls"
-                },
-                {
-                    value: 4,
-                    label: "Thin walls"
-                },
-                {
-                    value: 8,
-                    label: "Normal walls"
-                },
-                {
-                    value: 12,
-                    label: "Thick walls"
-                },
-                {
-                    value: 16,
-                    label: "Thickest walls"
-                }
-            ]} onChange={(value) => setWallThickness(value as number)}/>
+                <p>Customize your walls and floors! You can choose whether or not the walls of your room are visible, and how thick the walls and floors are.</p>
+            </div>
 
-            <Checkbox label="Hide room walls" value={wallHidden} onChange={setWallHidden}/>
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: 8
+            }}>
+                <b>Floor and walls</b>
+
+                <Selection value={floorThickness} items={[
+                    {
+                        value: 0,
+                        label: "Thinnest floor"
+                    },
+                    {
+                        value: 4,
+                        label: "Thin floor"
+                    },
+                    {
+                        value: 8,
+                        label: "Normal floor"
+                    },
+                    {
+                        value: 12,
+                        label: "Thick floor"
+                    },
+                    {
+                        value: 16,
+                        label: "Thickest floor"
+                    }
+                ]} onChange={(value) => setFloorThickness(value as number)}/>
+                
+                <Selection value={wallThickness} items={[
+                    {
+                        value: 0,
+                        label: "Thinnest walls"
+                    },
+                    {
+                        value: 4,
+                        label: "Thin walls"
+                    },
+                    {
+                        value: 8,
+                        label: "Normal walls"
+                    },
+                    {
+                        value: 12,
+                        label: "Thick walls"
+                    },
+                    {
+                        value: 16,
+                        label: "Thickest walls"
+                    }
+                ]} onChange={(value) => setWallThickness(value as number)}/>
+
+                <Checkbox label="Hide room walls" value={wallHidden} onChange={setWallHidden}/>
+            </div>
         </div>
     );
 }
