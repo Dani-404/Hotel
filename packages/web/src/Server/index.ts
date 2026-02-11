162ab28a9fc66472f2d7c6967ffb65a8b89bd915
Muff1n <253524507+muff1n-pixel@users.noreply.github.com>
2026-02-11 19:56:25 +0100

    chore: add web server port config

--- packages/web/src/Server/index.ts
+++ packages/web/src/Server/index.ts
@@ -170,6 +170,6 @@ app.post('/api/register', async (request, response) => {
     });
 });
 
-app.listen(3000);
+app.listen(config.port);
 
-console.log('Server listening on port 3000');
+console.log('Server listening on port ' + config.port);
