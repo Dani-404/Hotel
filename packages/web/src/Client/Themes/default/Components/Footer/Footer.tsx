cb4f4bf47c79d89d0d710b1413eaf897c3c404ab
Daniel <danielfigueiredo.12@hotmail.fr>
2026-02-22 09:56:12 +0100

    Using vite and initializing CMS Theme Manager. (#2)
    Co-authored-by: muff1n-pixel <253524507+muff1n-pixel@users.noreply.github.com>

---
+++ packages/web/src/Client/Themes/default/Components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer className='resize'>
+            Pixel63, a new Habbo Hotel R63B style web client.
+        </footer>
+    )
+}
+
+export default Footer;
